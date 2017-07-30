cfd09046d4f7acc498b99f48d6538ec8f7c4b246
Amazingandyyy <amazingandyyy@gmail.com>
2017-07-30 15:08:14 -0700

    2. update HelloEthSalon contract

--- contracts/HelloEthSalon.sol
+++ contracts/HelloEthSalon.sol
@@ -1,7 +1,14 @@
 pragma solidity ^0.4.4;
 
 contract HelloEthSalon {
+  string message = 'Hello Ethereum Salon!'
+  
   function HelloEthSalon() {
     // constructor
   }
+
+  function GetMessage() return (string) {
+    return message;
+  }
+  
 }
