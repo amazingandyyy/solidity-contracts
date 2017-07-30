7cd342ebc83dd7a874b6e0c09fcf4c0cfc330569
Amazingandyyy <amazingandyyy@gmail.com>
2017-07-30 15:17:48 -0700

    3. fix semicolon bug

--- contracts/HelloEthSalon.sol
+++ contracts/HelloEthSalon.sol
@@ -1,7 +1,7 @@
 pragma solidity ^0.4.4;
 
 contract HelloEthSalon {
-  string message = 'Hello Ethereum Salon!'
+  string message = 'Hello Ethereum Salon!';
   
   function HelloEthSalon() {
     // constructor
@@ -10,5 +10,5 @@ contract HelloEthSalon {
   function GetMessage() return (string) {
     return message;
   }
-  
+
 }
