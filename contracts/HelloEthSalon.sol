fda63f91aa43e8d4349f578a8bda9f0659a90c22
Amazingandyyy <amazingandyyy@gmail.com>
2017-08-01 21:48:56 -0700

    try 0.4.13 and 0.4.14

--- contracts/HelloEthSalon.sol
+++ contracts/HelloEthSalon.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.4;
+pragma solidity ^0.4.13;
 
 contract HelloEthSalon {
   string message = 'Hello Ethereum Salon!';
@@ -6,9 +6,8 @@ contract HelloEthSalon {
   function HelloEthSalon() {
     // constructor
   }
-
   function GetMessage() returns (string) {
     return message;
   }
-
+  
 }
