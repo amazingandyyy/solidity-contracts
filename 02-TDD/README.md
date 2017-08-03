2263d7d71ade398b91b82a0f88eb8f42500c16a8
Amazingandyyy <amazingandyyy@gmail.com>
2017-08-02 18:10:05 -0700

    finish 02 Testing

--- 02-TDD/README.md
+++
@@ -1,9 +0,0 @@
-### Test Driven Smart Contract deployment
-
-souces for [this medium article](https://medium.com/@amazingandyyy/tdd-smart-contract-472c2f73b4c3
-)
-
-#### author
-[amazingandyyy](https://github.com/amazingandyyy)
-
-
---
+++ 02-Testing/finish/test/hello_eth_salon.js
@@ -0,0 +1,14 @@
+var HelloEthSalon = artifacts.require('./HelloEthSalon.sol');
+
+contract('HelloEthSalon:GetMessage', function(accounts) {
+  it("should return a correct string", function(done) {
+    var hello_eth_salon = HelloEthSalon.deployed();
+    hello_eth_salon.then(function(contract){
+      return contract.GetMessage.call();
+    }).then(function(result){
+      cosnole.log(result)
+      assert.isTrue(result === 'Hello Ethereum Salon!');
+      done();
+    })
+  });
+});
